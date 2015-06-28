2ed324aee0e388dbafb06cd7f7f6d8de544ee08c
jiabing <jiabing@wolongdata.com>
2015-06-28 21:14:57 +0800

    继承wechat_sdk，添加了素材相关接口 待完成

--- basic.py
+++ basic.py
@@ -14,3 +14,44 @@ class WechatExtend(WechatBasic):
     '''
     def __init__(self, *args, **kwargs):
         super(WechatExtend, self).__init__(*args, **kwargs)
+
+    def get_material_list(self, type, offset=0, count=20):
+        '''
+        获取(永久)素材列表
+        '''
+        self._check_appid_appsecret()
+
+        return self._post(
+            url='https://api.weixin.qq.com/cgi-bin/material/batchget_material',
+            data={
+                'type': type,
+                'offset': offset,
+                'count': count
+            }
+        )
+
+    def add_permanent_material(self, articles):
+        '''
+        新增（永久）素材列表
+        articles示例:
+        articles = [
+            {
+                'title': xx,
+                'thumb_media_id': xx,
+                'author': xx,
+                'digest': xx,
+                'show_cover_pic': xx,
+                'content': xx,
+                'content_source_url': xx,
+            },
+            //如果是多图文，还有...
+        ]
+        '''
+        self._check_appid_appsecret()
+
+        return self._post(
+            url='https://api.weixin.qq.com/cgi-bin/material/add_news',
+            data={
+                'articles': articles
+            }
+        )
---
+++ const.py
@@ -0,0 +1,9 @@
+# -*- coding=utf-8 -*-
+# Created Time: 2015年06月18日 星期四 16时43分12秒
+# File Name: const.py
+
+from __future__ import unicode_literals
+
+appid = "wx2102221b928b1121"
+appsecret = "9606385cb216b2478e098de47e3f869d"
+token = "test"
