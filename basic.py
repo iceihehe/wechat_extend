7a5a5af085559cc5f6ac51d6aa5c0180379c3062
iceihehe <iceihehe@gmail.com>
2015-08-04 09:46:33 +0800

    完善某个接口

--- basic.py
+++ basic.py
@@ -5,6 +5,7 @@
 from __future__ import print_function, unicode_literals
 
 import requests
+import json
 
 from wechat_sdk import WechatBasic
 
@@ -16,6 +17,35 @@ class WechatExtend(WechatBasic):
     '''
     def __init__(self, *args, **kwargs):
         super(WechatExtend, self).__init__(*args, **kwargs)
+
+    def _request(self, method, url, **kwargs):
+        """
+        向微信服务器发送请求
+        :param method: 请求方法
+        :param url: 请求地址
+        :param kwargs: 附加数据
+        :return: 微信服务器响应的 json 数据
+        :raise HTTPError: 微信api http 请求失败
+        """
+        if "params" not in kwargs:
+            kwargs["params"] = {
+                "access_token": self.access_token,
+            }
+        if isinstance(kwargs.get("data", ""), dict):
+            if not kwargs.get('files'):
+                body = json.dumps(kwargs["data"], ensure_ascii=False)
+                body = body.encode('utf8')
+                kwargs["data"] = body
+
+        r = requests.request(
+            method=method,
+            url=url,
+            **kwargs
+        )
+        r.raise_for_status()
+        response_json = r.json()
+        self._check_official_error(response_json)
+        return response_json
 
     def get_material_list(self, media_type, offset=0, count=20):
         '''
@@ -39,7 +69,6 @@ class WechatExtend(WechatBasic):
         [
             {
                 'title': xx,
-            
                 'thumb_media_id': xx, //永久mediaID
                 'author': xx,
                 'digest': xx,
@@ -60,6 +89,7 @@ class WechatExtend(WechatBasic):
     def add_permanent_material(self, media_type, media_file):
         '''
         新增永久其他类型素材
+        media_file就是个file object
         '''
         self._check_appid_appsecret()
 
@@ -74,7 +104,7 @@ class WechatExtend(WechatBasic):
         }
         filename = media_file.name.split('/')[-1]
 
-        return requests.post(
+        return self._post(
             url='https://api.weixin.qq.com/cgi-bin/material/add_material',
             params={
                 'access_token': self.access_token,
@@ -92,7 +122,7 @@ class WechatExtend(WechatBasic):
         获取永久素材
         '''
         self._check_appid_appsecret()
-        
+
         return self._post(
             url='https://api.weixin.qq.com/cgi-bin/material/get_material',
             data={'media_id': media_id}
@@ -155,7 +185,8 @@ class WechatExtend(WechatBasic):
             data={'articles': articles}
         )
 
-    def preview(self, msgtype, user_id=None, user_name=None, media_id=None, content=None):
+    def preview(self, msgtype, user_id=None, user_name=None, media_id=None,
+                content=None):
         '''
         预览接口
         '''
@@ -189,38 +220,8 @@ class WechatExtend(WechatBasic):
             data=data
         )
 
-    def mass_send_by_openid(self, msgtype, user_ids, media_id=None, content=None):
-        '''
-        根据openid列表群发
-        '''
-        self._check_appid_appsecret()
-
-        some_types = [
-            'mpnews',
-            'voice',
-            'mpvideo',
-            'image',
-        ]
-
-        data = {'touser': user_id, 'msgtype': msgtype}
-
-        if msgtype in some_types:
-            data.update(
-                {msgtype: {'media_id': media_id}}
-            )
-        elif msgtype == 'text':
-            data.update(
-                {'text': {'content': content}}
-            )
-        elif msgtype == 'wxcard':
-            pass
-
-        return self._post(
-            url='https://api.weixin.qq.com/cgi-bin/message/mass/preview',
-            data=data
-        )
-
-    def mass_send_by_openid(self, msgtype, user_ids, media_id=None, content=None):
+    def mass_send_by_openid(self, msgtype, user_ids, media_id=None,
+                            content=None):
         '''
         根据openid列表群发
         user_ids是个列表
@@ -234,7 +235,7 @@ class WechatExtend(WechatBasic):
             'image',
         ]
 
-        data = {'touser': user_id, 'msgtype': msgtype, 'touser': user_ids}
+        data = {'touser': user_ids, 'msgtype': msgtype, 'touser': user_ids}
 
         if msgtype in some_types:
             data.update(
@@ -252,7 +253,8 @@ class WechatExtend(WechatBasic):
             data=data
         )
 
-    def mass_send_by_group(self, msgtype, is_to_all=False, group_id='0', media_id=None, title=None, content=None):
+    def mass_send_by_group(self, msgtype, is_to_all=False, group_id='0',
+                           media_id=None, title=None, content=None):
         '''
         根据组群发
         '''
